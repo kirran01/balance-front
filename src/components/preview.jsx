252e3991d18a8718cd49fd3367e15ecc0a4425e2
kirran01 <Kirran-@live.com>
2023-02-25 23:27:58 -0400

    Add new employee table

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Preview = ({ employee }) => {
     return (
         <div className='border-2 rounded-lg'>
-            <Link className='flex' to={'/employee/:id'}>
+            <Link className='flex' to={`/employee/${employee._id}`}>
                 <div>
                     <AccountCircleIcon />
                 </div>
                 <div className='flex'>
-                    <p>firstname</p>
-                    <p>lastname</p>
+                    <p className='mx-1'>{employee.firstName}</p>
+                    <p className='mx-1'>{employee.lastName}</p>
                 </div>
             </Link>
         </div>
--- src/pages/employees.jsx
+++ src/pages/employees.jsx
@@ -5,6 +5,21 @@ import { useState, useEffect } from 'react';
 
 const Employees = () => {
     const [employees, setEmployees] = useState([])
+    const[newEmployeeInput,setNewEmployeeInput]=useState({
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    image:''
+    })
+    const [show, setShow] = useState('')
+    const handleAddEmployee = () => {
+        if (show === '') {
+            setShow('add-employee')
+        } else {
+            setShow('')
+        }
+    }
     useEffect(() => {
         const getEmployees = async () => {
             try {
@@ -23,10 +38,29 @@ const Employees = () => {
         }
         getEmployees()
     }, [])
-    console.log(employees, 'e')
+
     return (
         <div className='flex flex-col items-center p-3'>
-            <h1 className='text-3xl'>Employees</h1>
+            <p className='text-4xl m-5'>Employees</p>
+            <div className='m-2'>
+                <button className='p-2 bg-slate-100 rounded-lg' onClick={handleAddEmployee}>Add New Employee</button>
+            </div>
+            {show === 'add-employee' && <div className='m-2 border-2 rounded-lg p-5 flex flex-col items-center'>
+                <form className='flex flex-col items-center'>
+                    <label>First Name</label>
+                    <input className='border-2' type="text" />
+                    <label>Last Name</label>
+                    <input className='border-2' type="text" />
+                    <label>Email</label>
+                    <input className='border-2' type="text" />
+                    <label>Phone</label>
+                    <input className='border-2' type="text" />
+                    <label>Image</label>
+                    <input className='border-2' type="text" />
+                    <button className='bg-green-300 border-2 border-green-400 rounded-lg m-2 p-3'>Submit</button>
+                </form>
+                <button className='bg-red-300 border-2 border-red-400 p-1 rounded-lg' type="text" onClick={handleAddEmployee}>Cancel</button>
+            </div>}
             <div>
                 {
                     employees.map(e => {
@@ -36,7 +70,7 @@ const Employees = () => {
                     })
                 }
             </div>
-        </div>
+        </div >
     );
 }
 
