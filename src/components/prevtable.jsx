5e8a0ef693ad52748db617d0cd6cb4e2bdabd456
kirran01 <Kirran-@live.com>
2023-03-04 21:30:35 -0400

    new edit ui

--- src/components/prevtable.jsx
+++ src/components/prevtable.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import { deleteModel } from 'mongoose';
 
 const Prevtable = ({ table, tables, setTables }) => {
-    const [extendEdit, setExtendEdit] = useState('')
+
+    const [editMode, setEditMode] = useState(false)
     const [fieldToEdit, setFieldToEdit] = useState('')
     const [userEditInput, setUserEditInput] = useState('')
     const updateTable = async (e) => {
@@ -41,45 +42,68 @@ const Prevtable = ({ table, tables, setTables }) => {
             console.log(err)
         }
     }
+    const handleEditMode = () => {
+        if (editMode) {
+            setEditMode(false)
+        } else {
+            setEditMode(true)
+        }
+    }
+    const openInput = (str) => {
+        if (editMode) {
+            setFieldToEdit(str)
+        }
+    }
 
     return (
-        <div className='flex border-2 border-slate-100 rounded-lg m-2 p-2'>
-            <button onClick={deleteTable}>Delete</button>
-            <div className='m-2'>
-                <div className='flex'>
-                    <p className='underline'>{new Date(table.createdOn).toDateString().substring(3)}</p>
+        <div className='flex flex-col border-2 border-slate-100 rounded-lg m-2 p-4'>
+            <div className='flex justify-between p-2'>
+                <button className='p-2 bg-slate-100 rounded-lg' onClick={deleteTable}>Delete</button>
+                <button className={`p-2 ${!editMode ? `bg-slate-100` : `bg-yellow-500`} rounded-lg`} onClick={handleEditMode}>Edit</button>
+            </div>
+            <div className='flex'>
+                <div className='m-2'>
+                    <div className='flex'>
+                        <p className='underline'>{new Date(table.createdOn).toDateString().substring(3)}</p>
+                    </div>
+                    <div className='flex'>
+                        <p className='cursor-pointer' onClick={() => { openInput('regularEarnings') }}>Regular Earnings: </p>
+                        <p className='text-green-500 ml-1'>{table.regularEarnings}</p>
+                        {fieldToEdit === 'regularEarnings' && <form className='flex'>
+                            <input placeholder={`regular earnings: ${table.regularEarnings}`} className='border-2 h-8 border-slate-100 rounded-lg' type="text" />
+                            <button className='p-2 bg-slate-100' type="">Submit</button>
+                            <button onClick={() => { setFieldToEdit('') }}>x</button>
+                        </form>}
+                    </div>
+                    <div className='flex'>
+                        <p>Overtime 1: </p>
+                        <p className='text-green-500 ml-1'>{table.overtimeOne}</p>
+                    </div>
+                    <div className='flex'>
+                        <p>Overtime 2: </p>
+                        <p className='text-green-500 ml-1'>{table.overtimeTwo}</p>
+                    </div>
                 </div>
-                <div className='flex'>
-                    <p>Regular Earnings: </p>
-                    <p className='text-green-500 ml-1'>{table.regularEarnings}</p>
-                </div>
-                <div className='flex'>
-                    <p>Overtime 1: </p>
-                    <p className='text-green-500 ml-1'>{table.overtimeOne}</p>
-                </div>
-                <div className='flex'>
-                    <p>Overtime 2: </p>
-                    <p className='text-green-500 ml-1'>{table.overtimeTwo}</p>
+                <div className='m-2'>
+                    <div className='flex'>
+                        <p>Health Surcharge:</p>
+                        <p className='text-red-400 ml-1'>{table.healthSurcharge}</p>
+                    </div>
+                    <div className='flex'>
+                        <p>National Insurance:</p>
+                        <p className='text-red-400 ml-1'>{table.nationalInsurance}</p>
+                    </div>
+                    <div className='flex'>
+                        <p>Paye:</p>
+                        <p className='text-red-400 ml-1'>{table.paye}</p>
+                    </div>
+                    <div className='flex'>
+                        <p>Other:</p>
+                        <p className='text-red-400 ml-1'>{table.other}</p>
+                    </div>
                 </div>
             </div>
-            <div className='m-2'>
-                <div className='flex'>
-                    <p>Health Surcharge:</p>
-                    <p className='text-red-400 ml-1'>{table.healthSurcharge}</p>
-                </div>
-                <div className='flex'>
-                    <p>National Insurance:</p>
-                    <p className='text-red-400 ml-1'>{table.nationalInsurance}</p>
-                </div>
-                <div className='flex'>
-                    <p>Paye:</p>
-                    <p className='text-red-400 ml-1'>{table.paye}</p>
-                </div>
-                <div className='flex'>
-                    <p>Other:</p>
-                    <p className='text-red-400 ml-1'>{table.other}</p>
-                </div>
-            </div>
+
         </div>
     );
 }
